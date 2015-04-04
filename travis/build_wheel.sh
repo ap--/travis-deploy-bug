712e9d87e9e056a933152e9ad75ff69e754aea47
ap-- <mail@andreaspoehlmann.de>
2015-04-04 13:26:56 +0200

    miniconda install ???

--- travis/build_wheel.sh
+++ travis/build_wheel.sh
@@ -6,7 +6,7 @@ set -e
 wget http://repo.continuum.io/miniconda/Miniconda-3.5.2-MacOSX-x86_64.sh \
     -O miniconda.sh
 chmod +x miniconda.sh
-./miniconda.sh -b
+#./miniconda.sh -b
 #export PATH=/Users/travis/miniconda/bin:$PATH
 #conda update --yes conda
 
